574f8e02c2c22419f3d817bd8801ff6cee850836
Fernando <seu@email.com>
2026-05-20 04:00:10 -0300

    Simulador Copa 2026 - versão inicial

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'copa2026-v4';
+const FILES = [
+  './simulador-copa-2026.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-maskable.svg',
+  './taca.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
